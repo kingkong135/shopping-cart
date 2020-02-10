384df29a13c80e05c2f6b34b7406dfb2f008eaa7
kingkong135 <nguyentiendat1531999@gmail.com>
2020-02-10 21:21:43 +0700

    feat: vuex form

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+new Vuex.Store({
+    state: { // data
+
+    },
+
+    getters: {
+        productCount () {
+
+        }
+    },
+
+    actions: {
+        fetchProduct () {
+            // make the call
+
+        }
+    },
+
+    mutations: {
+        setProducts () {
+            // update products
+        }
+    }
+
+
+})
